b20842a21bf5d80e4456f0415fef9f5ce4b47b0b
Ricardo Madarasz <madaraszricardo@gmail.com>
2022-02-18 12:53:04 +0100

    made the js file,fixed some stuff in css and html

---
+++ script/file.js
@@ -0,0 +1,35 @@
+var lilbaldy = 50;
+
+var krayzy = 70;
+
+var bikemike = 95;
+
+var artees = 15;
+
+var lilpedro = 100;
+
+var kardic = 333;
+
+var calkulaterr = 19;
+
+var uzihoris = 500;
+
+var trainwrekz = 299;
+
+
+function calculateInvoice (stage1, stage2, stage3) {
+    invoice = stage1 + stage2 + stage3;
+    return invoice;
+}
+// stage one
+console.log(calculateInvoice(lilbaldy, krayzy, bikemike));
+
+
+// stage two
+console.log(calculateInvoice(kardic, artees, lilpedro));
+
+
+// stage three
+console.log(calculateInvoice(calkulaterr, trainwrekz, uzihoris));
+
+
